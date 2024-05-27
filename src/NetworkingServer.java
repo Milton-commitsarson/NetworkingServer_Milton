0fb4ff458bbf0c0daab54004fb93656b09808aec
Milton-commitarson <milton.molanderryberg@elev.ga.ntig.se>
2024-05-27 16:19:09 +0200

    commit 2

--- src/NetworkingServer.java
+++ src/NetworkingServer.java
@@ -3,23 +3,23 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class NetworkingServer {
-    public static void main(String [] args){
-        ServerSocket server =  null;
+    public static void main(String[] args) {
+        ServerSocket server = null;
         Socket client;
 
         int portnummer = 1234;
-        if(args.length>= 1){
+        if (args.length >= 1) {
             portnummer = Integer.parseInt(args[0]);
         }
 
         try {
-server = new ServerSocket(portnummer);
+            server = new ServerSocket(portnummer);
         } catch (Exception e) {
             System.out.println("Cannot open socket." + ie);
             System.exit(1);
         }
-        System.out.println("Server socket is created "+ server);
-        while (true){
+        System.out.println("Server socket is created " + server);
+        while (true) {
             try {
                 System.out.println("Wating for connect request...");
                 client = server.accept();
@@ -28,11 +28,30 @@ server = new ServerSocket(portnummer);
                 String clientHost =
                         client.getInetAddress().getHostAddress();
                 int clientPort = client.getPort();
-                System.out.println("client host="  + clientHost +"Client port ="+clientPort);
-                InputStream
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+                System.out.println("client host=" + clientHost + "Client port =" + clientPort);
+
+                InputStream clientIn = client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
+
+                String msgFromClient = br.readLine();
+                System.out.println("Message received from client = " + msgFromClient);
+
+                if (msgFromClient != null && !msgFromClient.equalsIgnoreCase("bye")) {
+
+                    OutputStream clientOut = client.getOutputStream();
+                    PrintWriter pw = new PrintWriter(clientOut, true);
+                    String ansMsg = "Hello, " + msgFromClient;
+                    pw.println(ansMsg);
+                }
+
+                if (msgFromClient != null && msgFromClient.equalsIgnoreCase("bye")) {
+                    server.close();
+                    client.close();
+                    break;
+                }
+            } catch (IOException ie) {
             }
         }
     }
 }
+
